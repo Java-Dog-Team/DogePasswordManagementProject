e34aa4de6e4207c14362734d1de4e19df4e7bed5
23322231 <100283977+23322231@users.noreply.github.com>
2023-05-02 20:47:32 +0800

    login 頁面

---
+++ demo/src/main/java/com/example/login.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+public class login {
+    public static void main(String[] args) {
+        createWindow();
+    }
+    private static void createWindow() {    
+        JFrame frame = new JFrame("更改Swing視窗的預設圖示");           
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon arrowIcon = null;
+  
+        java.net.URL imgURL = SwingTester.class.getResource("dogdog.png");
+        if (imgURL != null) {
+           arrowIcon = new ImageIcon(imgURL);
+           frame.setIconImage(arrowIcon.getImage());
+        } else {
+           JOptionPane.showMessageDialog(frame, "Icon image not found.");
+        }
+  
+        createUI(frame);
+        frame.setSize(560, 200);      
+        frame.setLocationRelativeTo(null);  
+        frame.setVisible(true);
+     }
+  
+     private static void createUI(JFrame frame){      
+        JPanel panel = new JPanel();
+        LayoutManager layout = new FlowLayout();  
+        panel.setLayout(layout);       
+        panel.add(new JLabel("Welcome to Tw511.com!"));
+  
+        frame.getContentPane().add(panel, BorderLayout.CENTER);    
+     }
+}
