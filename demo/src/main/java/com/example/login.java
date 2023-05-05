1b97e2f0dea4c1189e21644ce6cf980a0c7cf2d7
April-ying <april910909@gmail.com>
2023-05-05 18:14:57 +0800

    狗狗位置靠左下 添加輸入欄位

--- demo/src/main/java/com/example/login.java
+++ demo/src/main/java/com/example/login.java
@@ -3,23 +3,26 @@ package com.example;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Label;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.awt.Dimension;
+import java.awt.Color;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class login {
     private static JFrame windowFrame = new JFrame("更改Swing視窗的預設圖示");
     //登入的背景圖片
-    private static ImageIcon dogPicture=new ImageIcon("demo\\src\\main\\java\\com\\example\\yellow_dog.jpeg");
+    private static ImageIcon dogPicture=new ImageIcon("demo\\src\\main\\java\\com\\example\\white_dog2.png");
     private static JLabel dog=new JLabel(dogPicture);
     private static Container container = windowFrame.getContentPane();
     //取得螢幕大小
@@ -30,7 +33,8 @@ public class login {
     private static void createWindow() {    
         //設定視窗大小為螢幕的2/3
         windowFrame.setSize(dimension.width*2/3, dimension.height*2/3);
-        
+        //設定背景顏色
+        windowFrame.getContentPane().setBackground( Color.WHITE );
         container.setLayout(new BorderLayout());
         //設定關閉可以關掉程式
         windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -49,7 +53,7 @@ public class login {
         createUI(windowFrame);
         //顯示圖片
         container.add(dog,BorderLayout.CENTER);
-        windowFrame.setSize(750, 400);      
+        //windowFrame.setSize(750, 400);      
 
         windowFrame.setLocationRelativeTo(null);  
         windowFrame.setVisible(true);
@@ -58,17 +62,42 @@ public class login {
     private static void putdog(JFrame windowFrame){
         int w=dogPicture.getIconWidth();
         int h=dogPicture.getIconHeight();
-        dogPicture.setImage(dogPicture.getImage().getScaledInstance(w/2, h/2, Image.SCALE_DEFAULT));
+        //縮小狗狗
+        dogPicture.setImage(dogPicture.getImage().getScaledInstance((int)(0.65*w), (int)(0.65*h), Image.SCALE_DEFAULT));
+        //視窗下緣和對齊
+        dog.setVerticalAlignment(JLabel.BOTTOM);
+        //視窗左邊和對齊
+        dog.setHorizontalAlignment(JLabel.LEFT);
         // dog.setLayout(new BorderLayout());
         // dog.setIcon(dogPicture);
-        dog.setSize(w/2, h/2);
+        //dog.setSize(w/2, h/2);
+
     }
 
     private static void createUI(JFrame windowFrame){
-        int w=dogPicture.getIconWidth();
-        int h=dogPicture.getIconHeight();
-        JLabel label=new JLabel("Login");
+        //int w=dogPicture.getIconWidth();
+        //int h=dogPicture.getIconHeight();
+        //JLabel label=new JLabel("Login");
+        JLabel label1=new JLabel("Login");
+        JLabel label2=new JLabel("Account:");
+        JLabel label3=new JLabel("Password:");
+        JTextField text1=new JTextField(null, "Anter your account", 0);//帳號輸入
+        JTextField text2=new JTextField(null, "Anter your password", 0);//密碼輸入
         // label.setSize(w/2, h/2);
-        dog.add(label,BorderLayout.SOUTH);
+        label1.setBounds(620, 90,200,100);
+        label2.setBounds(450, 150,90,90);
+        text1.setBounds(540, 180, 319, 30);
+        label3.setBounds(450, 200,200,100);
+        text2.setBounds(560, 235, 300, 30);
+
+        label1.setFont((new Font("",Font.ITALIC,35)));
+        label2.setFont((new Font("",Font.ITALIC,20)));
+        label3.setFont((new Font("",Font.ITALIC,20)));
+        windowFrame.add(label1);
+        windowFrame.add(label2);
+        windowFrame.add(text1);
+        windowFrame.add(label3);
+        windowFrame.add(text2);
+        //windowFrame.add(text,BorderLayout.SOUTH);
     }
 }
