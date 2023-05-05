150f0b85424232547081eeacb9c2a58c9d1d0727
23322231 <100283977+23322231@users.noreply.github.com>
2023-05-05 13:46:40 +0800

    試試看放文字在圖片上的左側 結果失敗QQ

--- demo/src/main/java/com/example/login.java
+++ demo/src/main/java/com/example/login.java
@@ -5,6 +5,7 @@ import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Label;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.awt.Dimension;
@@ -19,7 +20,7 @@ public class login {
     private static JFrame windowFrame = new JFrame("更改Swing視窗的預設圖示");
     //登入的背景圖片
     private static ImageIcon dogPicture=new ImageIcon("demo\\src\\main\\java\\com\\example\\yellow_dog.jpeg");
-    private static JLabel dog=new JLabel(dogPicture,JLabel.CENTER);
+    private static JLabel dog=new JLabel(dogPicture);
     private static Container container = windowFrame.getContentPane();
     //取得螢幕大小
     private static Dimension dimension=Toolkit.getDefaultToolkit().getScreenSize();
@@ -46,7 +47,8 @@ public class login {
 
         putdog(windowFrame);
         createUI(windowFrame);
-        
+        //顯示圖片
+        container.add(dog,BorderLayout.CENTER);
         windowFrame.setSize(750, 400);      
 
         windowFrame.setLocationRelativeTo(null);  
@@ -59,18 +61,14 @@ public class login {
         dogPicture.setImage(dogPicture.getImage().getScaledInstance(w/2, h/2, Image.SCALE_DEFAULT));
         // dog.setLayout(new BorderLayout());
         // dog.setIcon(dogPicture);
-        dog.setSize(dimension.width*2/3, dimension.height*2/3);
-        
-        //圖片應該會在中間阿QAQ
-        container.add(dog,BorderLayout.CENTER);
+        dog.setSize(w/2, h/2);
     }
 
-    private static void createUI(JFrame windowFrame){      
-        // JPanel panel = new JPanel();
-        // LayoutManager layout = new BorderLayout();  
-        // panel.setLayout(layout);       
-        // panel.add(new JLabel("hello world!"));
-
-        // windowFrame.getContentPane().add(panel, BorderLayout.CENTER);  
+    private static void createUI(JFrame windowFrame){
+        int w=dogPicture.getIconWidth();
+        int h=dogPicture.getIconHeight();
+        JLabel label=new JLabel("Login");
+        // label.setSize(w/2, h/2);
+        dog.add(label,BorderLayout.SOUTH);
     }
 }
