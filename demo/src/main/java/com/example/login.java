3aa74e9de21bc1375124aca4804f637a3772489f
April-ying <april910909@gmail.com>
2023-05-14 06:55:24 +0800

    圖片成功 文字還沒Q

--- demo/src/main/java/com/example/login.java
+++ demo/src/main/java/com/example/login.java
@@ -11,7 +11,8 @@ import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.awt.Dimension;
 import java.awt.Color;
-
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,32 +21,41 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 public class login {
     private static JFrame windowFrame = new JFrame("歡迎使用看門狗");
     //登入的背景圖片
-    private static ImageIcon dogPicture=new ImageIcon("demo\\src\\picture\\white_dog2.png");
+    private static ImageIcon dogPicture=new ImageIcon("demo\\src\\picture\\white_dog3.png");
     private static JLabel dog=new JLabel(dogPicture);
-    private static Container container = windowFrame.getContentPane();
-    //取得螢幕大小
-    private static Dimension dimension=Toolkit.getDefaultToolkit().getScreenSize();
-    private static JLabel frame_up=new JLabel(" ");
-    private static JLabel frame_left=new JLabel(" ");
-    private static JLabel frame_right=new JLabel(" ");
-    private static JLabel frame_down=new JLabel(" ");
+    private static JPanel panelBack=new JPanel();
     private static JPanel panelLeft=new JPanel();//
     private static JPanel panelRight=new JPanel();
+
+    //private static Container container = windowFrame.getContentPane();
+    //取得螢幕大小
+    private static Dimension dimension=Toolkit.getDefaultToolkit().getScreenSize();
+    private static JLabel frame_upL=new JLabel(" ");
+    private static JLabel frame_upR=new JLabel(" ");
+    private static JLabel frame_left=new JLabel(" ");
+    private static JLabel frame_right=new JLabel(" ");
+    private static JLabel frame_downL=new JLabel(" ");
+    private static JLabel frame_downR=new JLabel(" ");
+    private static GridBagConstraints c;   
     public static void main(String[] args) {
         createWindow();
     }
-    private static void createWindow() {    
+    private static void createWindow() { 
+        
         //設定視窗大小為螢幕的2/3
         windowFrame.setSize(dimension.width*2/3, dimension.height*2/3);
         //設定背景顏色
         windowFrame.getContentPane().setBackground( Color.WHITE );
         //container.setLayout(new BorderLayout());
         //設定關閉可以關掉程式
-        windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+        //把frame分成兩部分 放兩個Panel
+        windowFrame.setLayout(new GridLayout(1,2));
         
         //設置左上角小圖片
         ImageIcon arrowIcon = null;
@@ -57,36 +67,47 @@ public class login {
            JOptionPane.showMessageDialog(windowFrame, "Icon image not found.");
         }
 
+        panelLeft.setBorder(new LineBorder(Color.BLUE));//暫時先加邊框顏色 好區別
+        panelRight.setBorder(new LineBorder(Color.RED));//暫時先加邊框顏色 好區別
+
+        //設定背景顏色
+        panelLeft.setBackground(Color.WHITE);
+        panelRight.setBackground(Color.WHITE);
+
+        //設定兩個Panel的排版方式
+        panelLeft.setLayout(new BorderLayout());
+        panelRight.setLayout(new BorderLayout());
+        //邊框
+        yellow_frame(windowFrame);
+
+        //調整狗狗圖片大小 加入左邊的Panel
         putdog(windowFrame);
         createUI(windowFrame);
-        //顯示圖片
-        //container.add(dog,BorderLayout.CENTER);
         
+        //Panel加入windowFrame
+        windowFrame.add(panelLeft);
+        windowFrame.add(panelRight);
+
         windowFrame.setLocationRelativeTo(null);  
         windowFrame.setVisible(true);
+        
     }
 
     private static void putdog(JFrame windowFrame){
         int w=dogPicture.getIconWidth();
         int h=dogPicture.getIconHeight();
         //縮小狗狗
-        dogPicture.setImage(dogPicture.getImage().getScaledInstance((int)(0.55*w), (int)(0.55*h), Image.SCALE_DEFAULT));
-        panelLeft.add(dog);
-        container.add(panelLeft,BorderLayout.WEST);
-        
-        //視窗下緣和對齊
-        //dog.setVerticalAlignment(JLabel.BOTTOM);
-        //視窗左邊和對齊
-        //dog.setHorizontalAlignment(JLabel.LEFT);
+        dogPicture.setImage(dogPicture.getImage().getScaledInstance((int)(0.6*w), (int)(0.6*h), Image.SCALE_DEFAULT));
+        panelLeft.add(dog,BorderLayout.CENTER);
     }
 
     private static void createUI(JFrame windowFrame){
+        //panelRight.setLayout(new GridLayout(6, 1));
         JButton confirm=new JButton("Confirm");
         JLabel Login=new JLabel("Login");
         JLabel Account=new JLabel("Account:");
         JLabel Password=new JLabel("Password:");
-        JPanel panel=new JPanel();
-		panel.setLayout(new GridLayout(6,1));
+        
         //帳號輸入
         JTextField accountText=new JTextField(null, "", 0);
         //帳號提示語
@@ -98,62 +119,62 @@ public class login {
         //密碼框框文字會顯示
         passWord.setEchoChar('\0');
         
-        //各位置設定
-        // confirm.setBounds(615, 300, 80, 20);
-        // label1.setBounds(610, 90,200,100);
-        // label2.setBounds(440, 150,90,90);
-        // accountText.setBounds(530, 180, 319, 30);
-        // label3.setBounds(440, 200,200,100);
-        // passWord.setBounds(550, 235, 300, 30);
-
-        // Login.setFont((new Font("",Font.ITALIC,35)));
-        // Account.setFont((new Font("",Font.ITALIC,20)));
-        // Password.setFont((new Font("",Font.ITALIC,20)));
         
-        // panel.add(confirm);
-        // panel.add(Login);
-        // panel.add(Account);
-        // panel.add(accountText);
-        // panel.add(Password);
-        // panel.add(passWord);
+        Login.setBorder(new LineBorder(Color.BLACK));
+        Login.setPreferredSize(new Dimension(400, 50));
 
-        // windowFrame.add(panel,BorderLayout.EAST);
+        Account.setBorder(new LineBorder(Color.BLACK));
+        Account.setPreferredSize(new Dimension(500, 50));
 
-        panelRight.add(confirm);
-
-        //加入windowFrame
-        // windowFrame.add(confirm,BorderLayout.CENTER);
-        // windowFrame.add(Login);
-        // windowFrame.add(Account);
-        // windowFrame.add(accountText);
-        // windowFrame.add(Password);
-        // windowFrame.add(passWord);
-        // windowFrame.add(panel);
-        windowFrame.add(panelRight,BorderLayout.EAST);
-        yellow_frame(windowFrame);
+        Login.setFont((new Font("",Font.ITALIC,35)));
+        Account.setFont((new Font("",Font.ITALIC,20)));
+        Password.setFont((new Font("",Font.ITALIC,20)));
         
+
+        // panelRight.add(Login,BorderLayout.CENTER);
+        // panelRight.add(Account);
+        // panelRight.add(accountText);
+        // panelRight.add(Password);
+        // panelRight.add(passWord);
+        // panelRight.add(confirm);
     }
     //加上黃色邊框
     private static void yellow_frame(JFrame windowFrame){
-        //設定Lebel為不透明
-        frame_up.setOpaque(true); 
+        //黃色邊框(上(panelLeft))
+        frame_upL.setOpaque(true); 
+        frame_upL.setBackground(Color.orange);
+        frame_upL.setFont((new Font("",0,20)));
+        panelLeft.add(frame_upL,BorderLayout.NORTH);
+
+        //黃色邊框(上(panelRight))
+        frame_upR.setOpaque(true); 
+        frame_upR.setBackground(Color.orange);
+        frame_upR.setFont((new Font("",0,20)));
+        panelRight.add(frame_upR,BorderLayout.NORTH);
+
+        //黃色邊框(左)
         frame_left.setOpaque(true); 
-        frame_right.setOpaque(true); 
-        frame_down.setOpaque(true);
-        //設定Lebel顏色
-        frame_up.setBackground(Color.orange);
         frame_left.setBackground(Color.orange);
-        frame_right.setBackground(Color.orange);
-        frame_down.setBackground(Color.orange);
-        //設定大小
-        frame_up.setFont((new Font("",0,20)));
         frame_left.setFont((new Font("",0,100)));
+        panelLeft.add(frame_left,BorderLayout.WEST);
+
+        //黃色邊框(右)
+        frame_right.setOpaque(true); 
+        frame_right.setBackground(Color.orange);
         frame_right.setFont((new Font("",0,100)));
-        frame_down.setFont((new Font("",0,20)));
-        //加入windowFrame
-        windowFrame.add(frame_up,BorderLayout.NORTH);
-        windowFrame.add(frame_left,BorderLayout.WEST);
-        windowFrame.add(frame_right,BorderLayout.EAST);
-        windowFrame.add(frame_down,BorderLayout.SOUTH);
+        panelRight.add(frame_right,BorderLayout.EAST);
+
+        //黃色邊框(下(panelLeft))
+        frame_downL.setOpaque(true); 
+        frame_downL.setBackground(Color.orange);
+        frame_downL.setFont((new Font("",0,20)));
+        panelLeft.add(frame_downL,BorderLayout.SOUTH);
+
+        //黃色邊框(下(panelRight))
+        frame_downR.setOpaque(true); 
+        frame_downR.setBackground(Color.orange);
+        frame_downR.setFont((new Font("",0,20)));
+        panelRight.add(frame_downR,BorderLayout.SOUTH);
     }
 }
+
