956d2a69c0faa7daa0a63538843f8c2103115811
23322231 <100283977+23322231@users.noreply.github.com>
2023-05-24 17:14:57 +0800

    更改滑鼠事件 加上pressed的地方

--- demo/src/main/java/com/example/MouseTest.java
+++ demo/src/main/java/com/example/MouseTest.java
@@ -45,8 +45,7 @@ public class MouseTest extends JFrame{
         }
         @Override
         public void mouseEntered(MouseEvent e){  //當滑鼠游標進入物件範圍
-            int x = e.getX();  //擷取發生事件的 X 座標
-            int y = e.getY();  //擷取發生事件的 Y 座標
+
             
         }
         @Override
@@ -68,11 +67,6 @@ public class MouseTest extends JFrame{
         }
         @Override
         public void mousePressed(MouseEvent e){
-
-        }
-        @Override
-        public void mouseClicked(MouseEvent e){  //實做滑鼠的點擊事件
-            Graphics g = getGraphics();
             int x = e.getX();
             int y = e.getY();
             // System.out.printf("%d %d%n",x,y);
@@ -94,18 +88,40 @@ public class MouseTest extends JFrame{
             else if(y>=411 && y<=461 && x<=50){//使用教學
 
             }
+        }
+        @Override
+        public void mouseClicked(MouseEvent e){  //實做滑鼠的點擊事件
+            int x = e.getX();
+            int y = e.getY();
+            // System.out.printf("%d %d%n",x,y);
+            if(y>=27 && y<=73 && x<=150){//主頁面
+                home.creatPasswordPanel(passwardJPanel);
+                home.creatAddPasswardButton(addJPanel);
+            }
+            else if (y>=122 && y<=168 && x<=150){//密碼產生器
+                addJPanel.setVisible(false);
+                generate.createGenerate(passwardJPanel);
+            }
+            else if(y>=218 && y<=270 && x<=150){//提醒設定
+                addJPanel.setVisible(false);
+                remider.createReminder(passwardJPanel);
+            }
+            else if(y>=315 && y<=365 && x<=150){//主題設定
+                // DesignPage designPage=new DesignPage(jFrame);
+                // MainPage.jFrame.getContentPane().setBackground(Color.BLACK);
+                // System.out.printf("%d %d%n",x,y);
+            }
+            else if(y>=411 && y<=461 && x<=50){//使用教學
+
+            }
             
         }
         @Override
         public void mouseReleased(MouseEvent e){  //實做滑鼠的放開事件
-            Graphics g = getGraphics();
-            int x = e.getX();
-            int y = e.getY();
             
         }
         @Override
         public void mouseMoved(MouseEvent e){  //實做滑鼠的移動事件
-            Graphics g = getGraphics();
             int x = e.getX();
             int y = e.getY();
            //偵測滑鼠到左側選單按鈕，讓選單彈出
@@ -117,8 +133,6 @@ public class MouseTest extends JFrame{
                     sparklesIcon=new ImageIcon("demo\\src\\picture\\sparkles.png");
                     interrogationIcon=new ImageIcon("demo\\src\\picture\\interrogation.png");
                     setleftPanel();
-                    // Image image = ImageIO.read(new File("demo\\src\\picture\\home_end.jpg"));
-                    // g.drawImage(image, 7, 111, getFocusCycleRootAncestor());
                 }
                 catch(Exception ex){
                     System.out.println(ex);
@@ -133,8 +147,6 @@ public class MouseTest extends JFrame{
                         interrogationIcon=new ImageIcon("demo\\src\\picture\\interrogation.png");
 
                         setleftPanel();
-                        // Image image = ImageIO.read(new File("demo\\src\\picture\\lock_end.jpg"));
-                        // g.drawImage(image, 8, 208, getFocusCycleRootAncestor());
                     }
                     catch(Exception ex){
                         System.out.println(ex);
@@ -149,8 +161,6 @@ public class MouseTest extends JFrame{
                         interrogationIcon=new ImageIcon("demo\\src\\picture\\interrogation.png");
 
                         setleftPanel();
-                        // Image image = ImageIO.read(new File("demo\\src\\picture\\bell_end.jpg"));
-                        // g.drawImage(image, 7, 302, getFocusCycleRootAncestor());
                     }
                     catch(Exception ex){
                         System.out.println(ex);
@@ -165,8 +175,6 @@ public class MouseTest extends JFrame{
                         interrogationIcon=new ImageIcon("demo\\src\\picture\\interrogation.png");
 
                         setleftPanel();
-                        // Image image = ImageIO.read(new File("demo\\src\\picture\\sparkles_end.jpg"));
-                        // g.drawImage(image, 8, 400, getFocusCycleRootAncestor());
                     }
                     catch(Exception ex){
                         System.out.println(ex);
@@ -181,8 +189,6 @@ public class MouseTest extends JFrame{
                         interrogationIcon=new ImageIcon("demo\\src\\picture\\interrogation_end.jpg");
 
                         setleftPanel();
-                        // Image image = ImageIO.read(new File("demo\\src\\picture\\interrogation_end.jpg"));
-                        // g.drawImage(image, 8, 495, getFocusCycleRootAncestor());
                     }
                     catch(Exception ex){
                         System.out.println(ex);
@@ -191,33 +197,14 @@ public class MouseTest extends JFrame{
             else if(((y<=27 || y>=73) || (y<=122 || y>=168) || (y<=218 || y>=270) || (y<=315 || y>=365) || (y<=411 || y>=461)) && x>=143){
                 leftPanel.removeAll();
                 leftPanel.repaint();
-
                 homeIcon=new ImageIcon("demo\\src\\picture\\home.png");
                 lockIcon=new ImageIcon("demo\\src\\picture\\lock.png");
                 bellIcon=new ImageIcon("demo\\src\\picture\\bell.png");
                 sparklesIcon=new ImageIcon("demo\\src\\picture\\sparkles.png");
                 interrogationIcon=new ImageIcon("demo\\src\\picture\\interrogation.png");
-
                 setleftPanel();
-                
                 leftPanel.revalidate();
             }
-            
-            // else if(y<=93 && y>=143){
-            //     repaint(7,92,150,55);
-            // }
-            // else if(y<=172 && y>=221){
-            //     repaint(7,172,150,55);
-            // }
-            // else if(y<=250 && y>=300){
-            //     repaint(7,250,150,55);
-            // }
-            // else if(y<=331 && y>=379){
-            //     repaint(7,329,150,55);
-            // }
-            // else if(y<=409 && y>=456){
-            //     repaint(7,409,150,55);
-            // }
         }
         public void setleftPanel(){
             leftPanel.removeAll();
