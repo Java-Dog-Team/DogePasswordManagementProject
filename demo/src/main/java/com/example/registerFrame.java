05e3088b984aadd59aa854a6ff8e56e7698f8d34
April-ying <april910909@gmail.com>
2023-05-26 16:59:22 +0800

    補顯示密碼

--- demo/src/main/java/com/example/registerFrame.java
+++ demo/src/main/java/com/example/registerFrame.java
@@ -3,7 +3,8 @@ package com.example;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -26,6 +27,11 @@ public class registerFrame extends JFrame {
     private static JTextField validationCodeText = new JTextField(null, "", 0);// 驗證碼輸入欄位
     private static MailController mailController = new MailController();
 
+    private static Image dogImage=new ImageIcon("demo\\src\\picture\\dogdogdog.png").getImage();
+    private static ImageIcon dogIcon=new ImageIcon();
+    private static JButton showPwd1 = new JButton(dogIcon);
+    private static JButton showPwd2 = new JButton(dogIcon);
+
     public registerFrame() {
         super("Register Your Account");
         setLayout(null);
@@ -40,10 +46,19 @@ public class registerFrame extends JFrame {
         passWordText.setEchoChar('\0');
         passWordText2.setEchoChar('\0');
 
+        //圖片設定
+        dogImage=dogImage.getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+        dogIcon.setImage(dogImage);
+
         // 確認按鈕事件
         ButtonActionListener handler = new ButtonActionListener();
         confirm.addActionListener(handler);
         send.addActionListener(handler);
+        showPwd1.addActionListener(handler);
+        showPwd2.addActionListener(handler);
+
+        showPwd1.setToolTipText("Show password");
+        showPwd2.setToolTipText("Show password");
         // 各位置設定
         email.setBounds(100, 0, 180, 50);
         emailText.setBounds(100, 40, 300, 30);
@@ -51,12 +66,14 @@ public class registerFrame extends JFrame {
         phoneText.setBounds(100, 105, 300, 30);
         passWord.setBounds(100, 135, 180, 50);
         passWordText.setBounds(100, 170, 300, 30);
+        showPwd1.setBounds(410, 170, 26, 25);
         passWord2.setBounds(100, 200, 300, 50);
         passWordText2.setBounds(100, 235, 300, 30);
+        showPwd2.setBounds(410, 235, 26, 25);
         validationCodeLabel.setBounds(100, 270, 180, 50);
         validationCodeText.setBounds(100, 305, 300, 30);
-        confirm.setBounds(280, 340, 80, 20);
-        send.setBounds(110, 340, 150, 20);
+        confirm.setBounds(275, 340, 80, 20);
+        send.setBounds(140, 340, 100, 20);
         // 字型&大小設定
         email.setFont((new Font("", Font.ITALIC, 15)));
         passWord.setFont((new Font("", Font.ITALIC, 15)));
@@ -69,8 +86,10 @@ public class registerFrame extends JFrame {
         add(phoneText);
         add(passWord);
         add(passWordText);
+        add(showPwd1);
         add(passWord2);
         add(passWordText2);
+        add(showPwd2);
         add(validationCodeLabel);
         add(validationCodeText);
         add(send);
@@ -89,85 +108,110 @@ public class registerFrame extends JFrame {
             String PASSWORD2 = new String(passWordText2.getPassword());
             String PHONE = phoneText.getText().trim();
             String VALIDCOD = validationCodeText.getText().trim();
-            if ("Password:".equals(PASSWORD) || "Email:".equals(EMAIL) || "Password again:".equals(PASSWORD2)
-                    || "Phone number:".equals(PHONE)) {
-                JOptionPane.showMessageDialog(null, "Please enter complete information!!", "WARNING",
-                        JOptionPane.WARNING_MESSAGE);
-                return;
-            } else if (!(PASSWORD.equals(PASSWORD2))) {
-                JOptionPane.showMessageDialog(null, "Two password are different!!", "WARNING",
-                        JOptionPane.WARNING_MESSAGE);
-                return;
+
+            if(e.getSource()==showPwd1||e.getSource()==showPwd2){
+                if(e.getSource()==showPwd1){
+                    char echoChar = passWordText.getEchoChar();
+                    if (echoChar != 0) {
+                        passWordText.setEchoChar((char) 0);
+                    } 
+                    else {
+                        passWordText.setEchoChar('\u2022');
+                    }
+                }
+                else if(e.getSource()==showPwd2){
+                    char echoChar = passWordText2.getEchoChar();
+                    if (echoChar != 0) {
+                        passWordText2.setEchoChar((char) 0);
+                    } 
+                    else {
+                        passWordText2.setEchoChar('\u2022');
+                    }
+                }
             }
 
-            // 若文字框都有輸入且兩次密碼輸入皆相同
-            AccountController accountController = new AccountController();
+            else if(e.getSource()==confirm||e.getSource()==send){
+                if ("Password:".equals(PASSWORD) || "Email:".equals(EMAIL) || "Password again:".equals(PASSWORD2)
+                        || "Phone number:".equals(PHONE)) {
+                    JOptionPane.showMessageDialog(null, "Please enter complete information!!", "WARNING",
+                            JOptionPane.WARNING_MESSAGE);
+                    return;
+                } else if (!(PASSWORD.equals(PASSWORD2))) {
+                    JOptionPane.showMessageDialog(null, "Two password are different!!", "WARNING",
+                            JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
 
-            // 檢查帳號輸入格式
-            if (accountController.emailFormatCheck(EMAIL) == AccountController.INPUT_FORMAT_NOT_CORRECT) {// 若格式不為電子郵件
-                JOptionPane.showMessageDialog(null, "Email format is not correct,please enter again", "WARNING",
-                        JOptionPane.WARNING_MESSAGE);
-                return;
-            }
-            // 檢查手機號碼輸入格式
-            if (accountController.phoneFormatCheck(PHONE) == AccountController.INPUT_FORMAT_NOT_CORRECT) {// 若格式不為手機號碼
-                JOptionPane.showMessageDialog(null, "Phone number format is not correct,please enter again", "WARNING",
-                        JOptionPane.WARNING_MESSAGE);
-                return;
-            }
+                // 若文字框都有輸入且兩次密碼輸入皆相同
+                AccountController accountController = new AccountController();
 
-            // 檢查帳號是否重複
-            if (e.getActionCommand().equals("Send Code")) {
-                try {
-                    if (accountController.reapeatedAccount(EMAIL) == AccountController.USERNAME_REPEAT) {// 若帳號重複
-                        JOptionPane.showMessageDialog(null, "Email is already registered",
-                                "WARNING",
-                                JOptionPane.WARNING_MESSAGE);
-                        return;
-                    } else {// 帳號不重複 寄送驗證碼做電子郵件驗證
-                        if (mailController.SendRequest() == MailController.OK)
-                            mailController.sendMail(EMAIL);
-                        else {
-                            JOptionPane.showMessageDialog(null,
-                                    "Your verification code is already send,you can not send again in 5 minutes.",
+                // 檢查帳號輸入格式
+                if (accountController.emailFormatCheck(EMAIL) == AccountController.INPUT_FORMAT_NOT_CORRECT) {// 若格式不為電子郵件
+                    JOptionPane.showMessageDialog(null, "Email format is not correct,please enter again", "WARNING",
+                            JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
+                // 檢查手機號碼輸入格式
+                if (accountController.phoneFormatCheck(PHONE) == AccountController.INPUT_FORMAT_NOT_CORRECT) {// 若格式不為手機號碼
+                    JOptionPane.showMessageDialog(null, "Phone number format is not correct,please enter again", "WARNING",
+                            JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
+
+                // 檢查帳號是否重複
+                if (e.getActionCommand().equals("Send Code")) {
+                    try {
+                        if (accountController.reapeatedAccount(EMAIL) == AccountController.USERNAME_REPEAT) {// 若帳號重複
+                            JOptionPane.showMessageDialog(null, "Email is already registered",
+                                    "WARNING",
+                                    JOptionPane.WARNING_MESSAGE);
+                            return;
+                        } else {// 帳號不重複 寄送驗證碼做電子郵件驗證
+                            if (mailController.SendRequest() == MailController.OK)
+                                mailController.sendMail(EMAIL);
+                            else {
+                                JOptionPane.showMessageDialog(null,
+                                        "Your verification code is already send,you can not send again in 5 minutes.",
+                                        "SUCCESS",
+                                        JOptionPane.INFORMATION_MESSAGE);
+                                return;
+                            }
+
+                            JOptionPane.showMessageDialog(null, "Verification code send",
                                     "SUCCESS",
                                     JOptionPane.INFORMATION_MESSAGE);
                             return;
                         }
-
-                        JOptionPane.showMessageDialog(null, "Verification code send",
-                                "SUCCESS",
-                                JOptionPane.INFORMATION_MESSAGE);
-                        return;
-                    }
-                } catch (Exception e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                    return;
-                }
-            }
-            // 按下確認後檢查驗證碼輸入是否正確 正確的話將使用者資料上傳到資料庫
-            if (e.getActionCommand().equals("Confirm")) {
-                if (mailController.ValidCodeVerify(VALIDCOD) == AccountController.OK) {
-                    try {
-                        accountController.regiserAccount(EMAIL, PASSWORD, PHONE);
-                        JOptionPane.showMessageDialog(null, "Register success!",
-                                "SUCCESS",
-                                JOptionPane.INFORMATION_MESSAGE);
-                        close();// 關閉註冊葉面
-                        return;
                     } catch (Exception e1) {
                         // TODO Auto-generated catch block
                         e1.printStackTrace();
                         return;
                     }
-                } else {
-                    JOptionPane.showMessageDialog(null, "Verification code is not correct",
-                            "WARNING",
-                            JOptionPane.WARNING_MESSAGE);
-                    return;
+                }
+                // 按下確認後檢查驗證碼輸入是否正確 正確的話將使用者資料上傳到資料庫
+                if (e.getActionCommand().equals("Confirm")) {
+                    if (mailController.ValidCodeVerify(VALIDCOD) == AccountController.OK) {
+                        try {
+                            accountController.regiserAccount(EMAIL, PASSWORD, PHONE);
+                            JOptionPane.showMessageDialog(null, "Register success!",
+                                    "SUCCESS",
+                                    JOptionPane.INFORMATION_MESSAGE);
+                            close();// 關閉註冊葉面
+                            return;
+                        } catch (Exception e1) {
+                            // TODO Auto-generated catch block
+                            e1.printStackTrace();
+                            return;
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Verification code is not correct",
+                                "WARNING",
+                                JOptionPane.WARNING_MESSAGE);
+                        return;
+                    }
                 }
             }
+            
         }
     }
 }
